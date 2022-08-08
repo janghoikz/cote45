06e9c837991452f63820e6db40ed648fb8df26b5
ohne <ghdlr785@gmail.com>
2022-08-08 16:57:44 +0900

    초기 커밋.

---
+++ cote45/cote45.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 1234567890;
+    int temp = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        temp = a[i] * b[i];
+    }
+
+    answer += temp;
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
